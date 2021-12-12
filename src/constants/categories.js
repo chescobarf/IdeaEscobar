80f77236a99f04e5df9103366610609997e1967e
Christian Escobar F <christian17zxe@gmail.com>
2021-12-11 21:16:23 -0300

    Fix Categories

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -1,18 +1,26 @@
 export const categories = [
     {
         id: 1,
-        name: "Hombre"
+        name: "Hombre",
+        image: "https://picsum.photos/id/665/1500/500/",
+        description: "lorem ipsum lorem ipsum lorem ipsum"
     },
     {
         id: 2,
-        name: "Mujer"
+        name: "Mujer",
+        image: "https://picsum.photos/id/777/1500/500/",
+        description: "lorem ipsum lorem ipsum lorem ipsum"
     },
     {
         id: 3,
-        name: "Niños"
+        name: "Niños",
+        image: "https://picsum.photos/id/838/1500/500/",
+        description: "lorem ipsum lorem ipsum lorem ipsum"
     },
-    {
-        id: 4,
-        name: "Marcas"
-    }
+    // {
+    //     id: 4,
+    //     name: "Marcas",
+    //     image: "https://picsum.photos/id/690/1500/500/",
+    //     description: "lorem ipsum lorem ipsum lorem ipsum"
+    // }
 ]
