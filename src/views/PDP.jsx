80d771c65921ed8c52ed606a8ebbc8f76eb2bc26
Christian Escobar F <christian17zxe@gmail.com>
2021-12-11 21:16:55 -0300

    Fix useParams from react-router-dom

--- src/views/PDP.jsx
+++ src/views/PDP.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import ItemDetailContainer from "../components/ItemDetailContainer/ItemDetailContainer";
 
 function PDP() {
