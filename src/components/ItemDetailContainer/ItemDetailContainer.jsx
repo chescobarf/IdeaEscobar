79816724f987cf61bca64d523bf417fa89f30c30
Christian Escobar F <christian17zxe@gmail.com>
2022-01-05 21:55:26 -0300

    Feat Add Firebase Doc to show PDP

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,29 +2,36 @@ import { useEffect, useState } from "react";
 import { products } from "../../constants/products";
 import ItemDetail from "../ItemDetail/ItemDetail";
 import Loading from "../Loading/Loading";
+import { collection, getDoc, query, where, doc } from "firebase/firestore";
+import db from "../../service";
 
 function ItemDetailContainer({ id }) {
   const [state, setstate] = useState(null);
 
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(products[id - 1]);
-    }, 500);
-  });
+  // let promise = new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve(products[id - 1]);
+  //   }, 500);
+  // });
 
-  const getItem = async () => {
-    try {
-      const data = await promise;
-      setstate(data);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      console.log("Termine");
-    }
-  };
+  // const getItem = async () => {
+  //   try {
+  //     const data = await promise;
+  //     setstate(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     console.log("Termine");
+  //   }
+  // };
 
   useEffect(() => {
-    getItem();
+    const productRef = doc(db, "products", id);
+    getDoc(productRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        setstate({ ...snapshot.data() });
+      }
+    });
   });
   return (
     <div>
