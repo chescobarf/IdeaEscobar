4761501660ae15c70878af198885dc9dc6c571b1
Christian Escobar F <christian17zxe@gmail.com>
2022-01-18 22:36:26 -0300

    Update Tailwind, Height for Banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        'xl': '45rem',
+      }
+    }
   },
   variants: {
     extend: {},
