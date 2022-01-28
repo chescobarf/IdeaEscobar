4b7eea948110398c4332c35c2eaff24e1b4a2934
Christian Escobar F <christian17zxe@gmail.com>
2022-01-28 01:12:29 -0300

    Add Hover categoriesShelf

--- src/components/CategoryShelf/CategoryShelf.jsx
+++ src/components/CategoryShelf/CategoryShelf.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function CategoryShelf({ data }) {
   if (data) {
     return (
-      <div className="relative inline-block text-center">
+      <NavLink
+        to={`category/${data.name}`}
+        className="relative inline-block text-center hover:transform hover:scale-105"
+      >
         <img src={data.image} alt={data.name} />
         <span className="absolute top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl font-medium uppercase">
           {data.name}
         </span>
-      </div>
+      </NavLink>
     );
   } else {
     return (
